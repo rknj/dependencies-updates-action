f1282f3aeba2bcbda9ce5ff8d20fc75fada3551c
Michael de Checchi <mdechecchi@jahia.com>
2022-09-06 15:13:40 +0200

    feat: handle new and updated dependencies

--- src/modules/packages/analysePackage.ts
+++ src/modules/packages/analysePackage.ts
@@ -2,7 +2,12 @@ import GitHubClient from '../../services/github-sdk'
 import getLocalPackageInfo from './getLocalPackageInfo'
 import {DependenciesList} from '../../types/package'
 
-async function analysePackage(file: string): Promise<DependenciesList> {
+async function analysePackage(
+  file: string
+): Promise<{
+  newDependencies: DependenciesList
+  updatedDependencies: DependenciesList
+}> {
   const ghClient = GitHubClient.getClient()
 
   // fetches information about the package in the base branch
@@ -14,17 +19,38 @@ async function analysePackage(file: string): Promise<DependenciesList> {
     : []
 
   // fetches information about the updated package file
-  const addedPackage = await getLocalPackageInfo(file)
-  const addedDeps = Object.keys(addedPackage.dependencies)
-  const addedDevDeps = Object.keys(addedPackage.devDependencies)
+  const updatedPackage = await getLocalPackageInfo(file)
+  const updatedDeps = Object.keys(updatedPackage.dependencies)
+  const updatedDevDeps = Object.keys(updatedPackage.devDependencies)
 
   // filters new dependencies not existing in the base branch
-  const newDeps = addedDeps.filter(dep => !baseDeps.includes(dep))
-  const newDevDeps = addedDevDeps.filter(dep => !baseDevDeps.includes(dep))
+  const newDeps = updatedDeps.filter(dep => !baseDeps.includes(dep))
+  const newDevDeps = updatedDevDeps.filter(dep => !baseDevDeps.includes(dep))
 
-  return {
+  const upgradedDeps = Object.keys(
+    Object.entries(basePackage.dependencies).filter(
+      ([key, value]) => value !== updatedPackage.dependencies[key]
+    )
+  )
+
+  const upgradedDevDeps = Object.keys(
+    Object.entries(basePackage.devDependencies).filter(
+      ([key, value]) => value !== updatedPackage.devDependencies[key]
+    )
+  )
+
+  const newDependencies: DependenciesList = {
     dependencies: newDeps,
     devDependencies: newDevDeps
+  }
+  const updatedDependencies: DependenciesList = {
+    dependencies: upgradedDeps,
+    devDependencies: upgradedDevDeps
+  }
+
+  return {
+    newDependencies,
+    updatedDependencies
   }
 }
 
