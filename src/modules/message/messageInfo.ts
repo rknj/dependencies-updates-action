bdcb52ca1f9a62ab12b20064ecd1e8f8864e47e9
Michael de Checchi <mdechecchi@jahia.com>
2022-09-22 17:44:59 +0200

    fix: updated formatting

--- src/modules/message/messageInfo.ts
+++ src/modules/message/messageInfo.ts
@@ -1,9 +1,9 @@
 import {FullMetadata} from 'package-json'
 
 export const messageInfo = (dep: FullMetadata): string => {
-  return `\n| ${dep.homepage ? `[${dep.name}](${dep.homepage})` : dep.name} | |
+  return `\n| [${dep.name}](https://www.npmjs.com/package/${dep.name}) | |
 | Description | ${dep.description} |
 | Version | ${dep.version} |
 | License | ${dep.license} |
-| NPM | [${dep.name}](https://www.npmjs.com/package/${dep.name}) |`
+| Source | ${dep.homepage ? `[${dep.name}](${dep.homepage})` : dep.name} |`
 }
