8a2d463180e6a8de9e24550bbdb1b30fa43a7d2f
Michael de Checchi <mdechecchi@jahia.com>
2022-09-22 16:01:04 +0200

    fix: updated formatting

--- src/modules/message/messageInfo.ts
+++ src/modules/message/messageInfo.ts
@@ -2,33 +2,10 @@ import {FullMetadata} from 'package-json'
 
 export const messageInfo = (dep: FullMetadata): string => {
   return `
-${header(dep)}
-
-${table(dep)}
-
-${
-  dep.readme
-    ? `<details><summary>README.md</summary>${dep.readme}</details> `
-    : ``
-}
-`
-}
-
-const header = (dep: FullMetadata): string => {
-  return `
-### ${dep.homepage ? `[${dep.name}](${dep.homepage})` : dep.name}
-
-`
-}
-
-const table = (dep: FullMetadata): string => {
-  return `
-| Field | Value |
-| ----------- | ------------------ |
+| ${dep.homepage ? `[${dep.name}](${dep.homepage})` : dep.name} | |
 | Description | ${dep.description} |
 | Version | ${dep.version} |
 | License | ${dep.license} |
-| Created on | ${dep.time?.created} |
-| Last modified | ${dep.time?.modified} |
+| NPM | [${dep.name}](https://www.npmjs.com/package/${dep.name} |
 `
 }
