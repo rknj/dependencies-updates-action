c54051804cf6fdb282615907ff737e00b601d30a
Michael de Checchi <mdechecchi@jahia.com>
2022-10-06 11:54:29 +0200

    fix: updated formatting to have 1 line for 1 lib

--- src/modules/message/messageInfo.ts
+++ src/modules/message/messageInfo.ts
@@ -1,9 +1,9 @@
 import {FullMetadata} from 'package-json'
 
 export const messageInfo = (dep: FullMetadata): string => {
-  return `| [${dep.name}](https://www.npmjs.com/package/${dep.name}) | |
-| Description | ${dep.description} |
-| Version | ${dep.version} |
-| License | ${dep.license} |
-| Source | ${dep.homepage ? `[${dep.name}](${dep.homepage})` : dep.name} |`
+  return `| [${dep.name}](https://www.npmjs.com/package/${dep.name}) | ${
+    dep.description
+  } | ${dep.version} | ${dep.license} | ${
+    dep.homepage ? `[${dep.name}](${dep.homepage})` : dep.name
+  } |`
 }
